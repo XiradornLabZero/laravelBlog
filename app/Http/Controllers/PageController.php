4d66da5cbf700af36b4199d1567e2978c7e4d854
Xiradorn <xiradorn@gmail.com>
2017-02-02 18:48:40 +0100

    Commit for Lesson 5 with few notes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -16,14 +16,72 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller {
 
+	/**
+	 * Function created for generate home page structure 
+	 * @return view 
+	 */
 	public function getHome() {
 		return view('pages.welcome');
 	}
 
+	/**
+	 * Function created for generate about page structure 
+	 * @return view 
+	 */
 	public function getAbout() {
-		return view('pages.about');
+		$name = "Tony";
+		$surname = "Frost";
+
+		# First solution for pass variable data into view
+		// $full = "{$name} {$surname}";
+		// return view('pages.about')->with("fullname", $full);
+		
+		# Second solution for pass variable data into view
+		// $fullname = "{$name} {$surname}";
+		// return view('pages.about')->withFullname($fullname);
+		
+		# Third solution for pass variable data into view
+		# used for multiple vars
+		// $fullname = "{$name} {$surname}";
+		// $email = "gattoVerdone@example.com";
+		// return view('pages.about')->withFullname($fullname)->withEmail($email);
+
+		# Forth solution for pass variable data into view
+		# used for multiple vars
+		// $fullname = "{$name} {$surname}";
+		// $email = "gattoVerdone@example.com";
+
+		// # now we use data var for grab value
+		// $data = [
+		// 	'email' 	=> $email,
+		// 	'fullname' 	=> $fullname
+		// ];
+
+		// return view('pages.about')->withData($data);
+		// or
+		// return view('pages.about', $data);
+		
+		# Five solution for pass variable data into view
+		# used for multiple vars
+		# using native php compact function
+		$fullname = "{$name} {$surname}";
+		$email = "gattoVerdone@example.com";
+
+		// return view('pages.about', compact("fullname", "email"));
+		// Alternative
+		$data = [
+			'email',
+			'fullname'
+		];
+
+		return view('pages.about', compact($data));
+
 	}
 
+	/**
+	 * Function created for generate contact page structure 
+	 * @return view 
+	 */
 	public function getContact() {
 		return view('pages.contact');
 	}
