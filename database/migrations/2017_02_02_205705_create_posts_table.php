7148085ef21d33371b4681c1f31806d5e2488037
Xiradorn <xiradorn@gmail.com>
2017-02-03 14:38:01 +0100

    Lesson 9 - Migration create and Fix

--- database/migrations/2017_02_02_205705_create_posts_table.php
+++ database/migrations/2017_02_02_205705_create_posts_table.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * Added for build the schema for posts table. 
+ */
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,7 +17,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title'); /*i can add for example a defalu value white ->default('No Title')*/;
+            $table->text('body');
             $table->timestamps();
+            // $table->integer('tags')->after('body') if i want to move this after body
         });
     }
 
