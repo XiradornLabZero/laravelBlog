225a352b1e6dd03b672a99182af60bb66f20fe30
Xiradorn <xiradorn@gmail.com>
2017-02-04 01:14:57 +0100

    Lesson 15 Date, Edit, Destroy in Blade Only

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,38 @@
 
 @section('content')
 
-    <h1>{{ $post->title }}</h1>
+    <div class="row">
+        <div class="col-md-8">
 
-    <p class="lead">{{ $post->body }}</p>
+            <h1>{{ $post->title }}</h1>
+            <p class="lead">{{ $post->body }}</p>
+
+        </div>
+
+        <div class="col-md-4">
+            <div class="well">
+                <dl class="dl-horizontal">
+                    <dt>Created at:</dt>
+                    <dd>{{ date('j M Y \a\t H:i', strtotime($post->created_at)) }}</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>Last Update:</dt>
+                    <dd>{{ date('j M Y \a\t H:i', strtotime($post->updated_at)) }}</dd>
+                </dl>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-6">
+                        {{-- Four parameters: Route, The Value of the button, Id of the post into ARRAY, optional parameters --}}
+                        {!! Html::linkRoute('posts.edit', 'Edit', [$post->id], ['class' => 'btn btn-primary btn-block']) !!}
+                        {{-- <a href="#" class="btn btn-primary btn-block">Edit</a> --}}
+                    </div>
+                    <div class="col-sm-6">
+                        {{-- <a href="#" class="btn btn-danger btn-block">Delete</a> --}}
+                        {!! Html::linkRoute('posts.destroy', 'Delete', [$post->id], ['class' => 'btn btn-danger btn-block']) !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
